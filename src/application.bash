360160879100c3b7ea10dbad6a665854a3817199
Matheo Epie <matheoepie@outlook.com>
2019-02-08 10:55:20 +0100

    add var to script

--- src/application.bash
+++ src/application.bash
@@ -1,4 +1,6 @@
 ### Application.bash - 2019 - Epie ###
-echo "Copyright Company - 2019"
-echo "Build : 10:00"
-echo "version 1.1"
+version = "2"
+build_hour = "11H00"
+
+echo "Build ${build_hour}"
+echo "version ${version}"
